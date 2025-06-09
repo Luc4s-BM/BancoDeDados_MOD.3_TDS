e78b48bdb199cddb3b3597e4516fff2b195b8392
Lucas Balen Mattos <balenmattoslucas12@gmail.com>
2025-06-09 17:21:08 -0300

    COMMIT 0906

---
+++ modulo3/listacinema.php
@@ -0,0 +1,30 @@
+<?php
+include 'conectabanco.php';
+echo'
+<html>
+<h2>Listagem de cinemas</h2>
+<table border=2>
+    <tr aling="center">
+        <th>ID Cinema</thaling=>
+        <th>Nome</thing=>
+        <th>Cidade</th>
+    </tr>
+';
+ 
+try {
+ $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $sql='SELECT * FROM cinemaarthur ';
+ $data = $conn->query($sql);
+ foreach($data as $row)
+ {
+   echo'<td> '.($row[0]).'</td>';
+   echo'<td> '.($row[1]).'</td>';
+   echo'<td> '.($row[2]).'</td></tr>';
+ }
+ echo'</table>
+ </html>';
+} catch(PDOException $e) {
+ echo 'ERROR: ' . $e->getMessage();
+}
+ ?>
